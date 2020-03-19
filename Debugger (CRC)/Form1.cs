79f47c961394024b35488f517733019aeb854c8f
PICCIN <Celestin.PICCIN@cpnv.ch>
2020-03-19 16:10:04 +0100

    exercice debug CRC

---
+++ Debugger (CRC)/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+// Programme: Calcul de CRC sur un numéro de carte de crédit
+// Auteur:    X. Carrel
+// Date:      Mars 2013
+
+namespace Debugger__CRC_
+{
+    public partial class frmCRC : Form
+    {
+        const int NbChiffresBase = 18;    // Le nombre de chiffres dans le numéro sans CRC
+        long CRCVal = 0; // CRC (cumul)
+
+        public frmCRC()
+        {
+            InitializeComponent();
+        }
+
+        private long CRC(string Donnée)
+        // Méthode qui calcule le CRC d'une donnée.
+        {
+            int     NbChiffres = 0; // Pour compter le nombre de chiffres dans le numéro fourni
+            CRCVal = 0; //(BUG 1) IL FAUT AJOUTER CA 
+
+            // Parcourir la donnée
+            for (int i = 0; i < Donnée.Length; i++)
+            {
+                char c = Donnée[i];
+                if ((c >= '0') && (c <= '9')) // C'est un chiffre
+                //(BUG 2) IL FAUT METTRE >='0' ET <='9' SINON LE PROGRAMME NE PREND PAS EN COMPTE LES 0 ET LES 9
+                {
+                    NbChiffres++;
+                    CRCVal = CRCVal + ((int)c - (int)'0');
+                    if (CRCVal >= 100) // On ne peut pas dépasser 100 parce qu'on n'a que deux chiffres pour le CRC
+                    //(BUG 3) MEME CHOSE QUE BUG 2, IL FAUT METTRE >= SINON LE SINON NUMERO 100 N'EST PAS PRIS EN COMPTE DANS LES NUMEROS A PLUS DE 2 CHIFFRES
+                        CRCVal = CRCVal - 100;
+                }
+            }
+            if (NbChiffres != NbChiffresBase)
+            {
+                MessageBox.Show(string.Format("Erreur: un numéro de carte doit contenir {0} chiffres (sans le CRC)",NbChiffresBase));
+                return -1;
+            }
+            return CRCVal;
+        }
+
+        private void cmdCheckCRC_Click(object sender, EventArgs e)
+        {
+            string Num = txtData.Text;
+
+            if (Num == "")
+            {
+                MessageBox.Show("Introduisez un numéro de carte SVP");
+                return;
+            }
+
+            long CRCVal;
+
+            if (rbtCalcul.Checked) // On cherche à calculer un CRC
+            {
+                CRCVal = CRC(Num); // Calcul du CRC
+                if (CRCVal >= 0)
+                    MessageBox.Show("Le CRC vaut: " + CRCVal.ToString());
+            }
+            else // On vérifie un numéro complet
+            {
+                int CRCIntro = 10 * ((int)Num[Num.Length - 2] - (int)'0') + ((int)Num[Num.Length - 1] - (int)'0'); // Le CRC inclus dans le numéro (deux derniers chiffres)
+                Num = Num.Substring(0, Num.Length - 2); // On enlève les deux derniers chiffres
+                CRCVal = CRC(Num); // et on calcule le CRC
+
+                if (CRCIntro == CRCVal)
+                    MessageBox.Show("Le numéro est valide");
+                else
+                    MessageBox.Show("Le numéro n'est pas valide");
+            }
+        }
+
+        private void frmCRC_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
